a71fc8e051d67d94e39bd855324b8cc7d5bb02fa
Scooby <tmnsparky@hotmail.co.uk>
2026-06-13 22:55:08 +0100

    Update version and name for Jarvis Bot

--- Jarvis.meta.js
+++ Jarvis.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Jarvis Bot 2000.162
+// @name         Jarvis Bot 2000.163
 // @namespace    http://tampermonkey.net/
-// @version      2000.162
-// @description  Jarvis Bot 2000.162 — automated game assistant with Office-style UI, light/dark theme, Telegram alerts, OC/DTM auto-accept, online watch, garage management
+// @version      2000.163
+// @description  Jarvis Bot 2000.163 — automated game assistant with Office-style UI, light/dark theme, Telegram alerts, OC/DTM auto-accept, online watch, garage management
 // @author       Jarvis
 // @match        *://www.tmn2010.net/login.aspx*
 // @match        *://www.tmn2010.net/authenticated/*
