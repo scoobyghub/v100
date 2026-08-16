f0b825f12c28b3c76ccefd5153e97bfadb71fe0a
Scooby <tmnsparky@hotmail.co.uk>
2026-08-16 15:46:29 +0100

    Update version and add Discord connection

--- Jarvis.meta.js
+++ Jarvis.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Jarvis Bot
 // @namespace    http://tampermonkey.net/
-// @version      2000.247
+// @version      2000.253
 // @description  Jarvis Bot — automated game assistant with Office-style UI, light/dark theme, Telegram alerts, OC/DTM auto-accept, online watch, garage management
 // @author       Jarvis
 // @match        *://www.tmn2010.net/login.aspx*
@@ -26,6 +26,7 @@
 // @grant        GM_addStyle
 // @grant        GM_xmlhttpRequest
 // @connect      api.telegram.org
+// @connect      discord.com
 // @connect      raw.githubusercontent.com
 // @connect      starvinggeeks.net
 // @updateURL    https://raw.githubusercontent.com/scoobyghub/v100/refs/heads/main/Jarvis.meta.js
